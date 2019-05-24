bb1320164132b5b921b9a35411901a447ace5ab8
Jessie Leung <jleu1656@gmail.com>
2019-05-24 14:40:59 +1000

    Fix eslint config

--- src/app.js
+++ src/app.js
@@ -5,10 +5,9 @@ import React from 'react';
 function App() {
     return (
         <div className="App">
-            This is the app
+            This is the app!!!!
         </div>
     );
   }
   
   export default hot(App);
-  
